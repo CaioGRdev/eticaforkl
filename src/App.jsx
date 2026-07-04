a80218b56b60f6b79e1d0ce296b9290969b0ff48
Caio <153438743+CaioGRdev@users.noreply.github.com>
2026-07-04 09:28:09 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import Home from './pages/Home';
 import QuemSomos from './pages/QuemSomos';
 
